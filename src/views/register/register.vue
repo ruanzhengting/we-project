1f1efa435a202d8e1ee044a81fcb02f8afa1f19a
liuxinran123 <3059657940@qq.com>
2023-09-13 16:53:25 +0800

    刘欣冉

--- src/views/register/register.vue
+++ src/views/register/register.vue
@@ -1,13 +1,69 @@
 <template>
   <div id='register'>
-   <div class="top"></div>
-   <div class="header">
-    <div class="icon"></div>
-    <p class="name">MY-Mall</p>
-   </div>
-   <div class="content">
-    <div class="list"></div>
-   </div>
+    <div class="top"></div>
+    <div class="header">
+      <div class="icon"><i class="el-icon-goods"></i></div>
+      <p class="name">MY-Mall</p>
+    </div>
+    <div class="content">
+      <div class="list">
+        <div class="title">新用户注册</div>
+        <div class="item">
+          <ul>
+            <li>
+              <div class="icon"><i class="el-icon-user-solid"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入用户名">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-lock"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入密码">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-lock"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请再次输入密码">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-phone"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入电话">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-message"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入邮箱">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-question"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入密码提示问题">
+              </div>
+            </li>
+            <li>
+              <div class="icon"><i class="el-icon-key"></i></div>
+              <div class="inp">
+                <input type="text" placeholder="请输入密码提示答案">
+              </div>
+            </li>
+          </ul>
+          <button class="btn">立即注册</button>
+          <div class="box">
+            <router-link to="path">已有账号，去注册>></router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="text">幸福大学<span>|</span>图书馆<span>|</span>快乐学院</div>
+      <div class="date">Copyright 2023 Mall All rights Reserved</div>
+    </div>
   </div>
 </template>
 
@@ -55,8 +111,10 @@ export default {
 </script>
 
 <style scoped lang="less">
+*{margin: 0;padding: 0;}
+ul{list-style: none;}
 #register{
-  width: 1440px;
+  // width: 1440px;
   .top{
     width: 100%;
     height: 30px;
@@ -71,9 +129,15 @@ export default {
     .icon{
       width: 40px;
       height: 40px;
+      line-height: 40px;
+      text-align: center;
       border-radius: 50%;
       background-color: #1b598b;
       margin-right: 12px;
+      i{
+        font-size: 22px;
+        color: #fff;
+      }
     }
     .name{
       color: #1b598b;
@@ -85,6 +149,82 @@ export default {
     width: 100%;
     height: 700px;
     background-color: #1b598b;
+    display: inline-block;
+    .list{
+      width: 390px;
+      height: 620px;
+      background-color: #fff;
+      margin: 40px auto;
+      .title{
+        width: 100%;
+        height: 59px;
+        line-height: 59px;
+        text-align: center;
+        border-bottom: 1px solid #c2c7cc;
+        font-size: 20px;
+        font-weight: 700;
+      }
+      .item{
+        padding: 20px 0 20px 23px;
+        ul li{
+          width: 344px;
+          height: 38px;
+          margin-bottom: 20px;
+          border: 1px solid #c2c7cc;
+          display: flex;
+          align-items: center;
+          .icon{
+            width: 38px;
+            height: 38px;
+            text-align: center;
+            line-height: 38px;
+            background-color: #f4f4f4;
+            border-right: 1px solid #c2c7cc;
+            color: #8e8e8e;
+          }
+          .inp{
+            width: 297px;
+            input{
+              border: none;
+              outline: none;
+              height: 38px;
+              width: 100%;
+              padding-left: 8px;
+              color: #8e8e8e;
+            }
+          }
+        }
+        .btn{
+          width: 344px;
+          height: 40px;
+          background-color: #215496;
+          color: #fff;
+          border: none;
+          font-size: 16px;
+          margin-bottom: 28px;
+        }
+        .box{
+          display: flex;
+          justify-content: end;
+          margin-right: 21px;
+          a{
+            text-decoration: none;
+            color: #215496;
+            font-size: 14px;
+          }
+        }
+      }
+    }
+  }
+  .bottom{
+    width: 100%;
+    text-align: center;
+    font-size: 13px;
+    color: #727272;
+    padding-top: 32px;
+    .text{
+
+    }
   }
 }
 </style>
