8a46be11a2b42ec3dbb24344bdbc53dcc6213c97
15530653995 <3477143695@qq.com>
2023-09-13 14:16:28 +0800

    颜蕊文件

--- src/views/login/loginPage.vue
+++ src/views/login/loginPage.vue
@@ -1,31 +1,45 @@
 <template>
- <div id='loginPage'>
-  <div class="head_list"></div>
-  <div class="head_tit">
-    <div class="d_logo">
-      <i class="el-icon-goods"></i>
-    </div>
-    <p class="tit_p">MY-Mall</p>
-  </div>
-  <div class="content">
-    <div class="cont_it">
-      <div class="it_tit">
-        <p class="Name">用户登录</p>
+  <div id='loginPage'>
+    <div class="head_list"></div>
+    <div class="head_tit">
+      <div class="d_logo">
+        <i class="el-icon-goods"></i>
       </div>
-      <div class="it_user">
-        <div class="it_us_item">
-          <i class="el-icon-user-solid"></i>
+      <p class="tit_p">MY-Mall</p>
+    </div>
+    <div class="content">
+      <div class="cont_it">
+        <div class="it_tit">
+          <p class="Name">用户登录</p>
         </div>
-        <input type="text" placeholder="请输入用户名">
+        <div class="it_user">
+          <div class="it_us_item">
+            <i class="el-icon-user-solid"></i>
+          </div>
+          <input type="text" placeholder="请输入用户名">
+        </div>
+        <div class="it_pass">
+          <div class="it_us_item">
+            <i class="el-icon-s-tools"></i>
+          </div>
+          <input type="password" placeholder="请输入密码">
+        </div>
+        <div class="it_dl">登录</div>
+        <div class="it_bt">
+          <span>忘记密码</span>
+          <span>免费注册</span>
+        </div>
       </div>
-      <div class="it_pass">
-        <i class="el-icon-s-tools"></i>
-        <input type="password" placeholder="请输入密码">
+    </div>
+    <div class="bottom_btn">
+      <div class="_t">
+        幸福大学<span>|</span>
+        图书馆<span>|</span>
+        快乐学院
       </div>
-      <div class="it_dl">登录</div>
+      <div class="_b">Copyright 2023 Mall All rights Reserved</div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
