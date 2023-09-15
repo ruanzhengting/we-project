9756aa8da6af71b38145a1b1c381b6ff27261d10
SunYuHang11 <2380815741@qq.com>
2023-09-15 16:17:27 +0800

    孙宇航

--- src/components/bottomModel.vue
+++ src/components/bottomModel.vue
@@ -14,13 +14,13 @@
 <script>
 export default {
   name: 'bottomModel',
-  //import引入组件才能使用
+  // import引入组件才能使用
   components: {},
   props: {},
   data () {
     return {
 
-    };
+    }
   },
   // 计算属性
   computed: {},
@@ -38,13 +38,13 @@ export default {
   mounted () {
 
   },
-  beforeCreate () { }, //生命周期：创建之前
-  beforeMount () { }, //生命周期：挂载之前
-  beforeUpdate () { }, //生命周期：更新之前
-  updated () { }, //生命周期：更新之后
-  beforeDestroy () { }, //生命周期：销毁之前
-  destroyed () { }, //生命周期：销毁完成
-  activated () { }, //如果页面有keep-alive缓存功能,这个函数会触发执行
+  beforeCreate () { }, // 生命周期：创建之前
+  beforeMount () { }, // 生命周期：挂载之前
+  beforeUpdate () { }, // 生命周期：更新之前
+  updated () { }, // 生命周期：更新之后
+  beforeDestroy () { }, // 生命周期：销毁之前
+  destroyed () { }, // 生命周期：销毁完成
+  activated () { } // 如果页面有keep-alive缓存功能,这个函数会触发执行
 }
 </script>
 <style scoped lang="less">
